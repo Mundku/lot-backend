e988b9ea5792c0b06d7e2c21d2da0fa38ce23ff2
Mundku <mmundku@gmail.com>
2026-04-14 16:19:01 +0800

    Fix exports in authController and add missing utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,24 @@
-export const register = asyncHandler(async (req, res, next) => { // next нэмсэн
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/User.js';
+import generateToken from '../utils/generateToken.js';
+
+export const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      role: user.role,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(401);
+    throw new Error('Нэвтрэх нэр эсвэл нууц үг буруу');
+  }
+});
+
+export const register = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
   const userExists = await User.findOne({ username });
   if (userExists) {
