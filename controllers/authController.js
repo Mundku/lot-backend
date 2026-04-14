a6441537df6a89c04323685135b90722ddbc3905
Mundku <mmundku@gmail.com>
2026-04-14 16:36:25 +0800

    Remove asyncHandler, use try/catch directly

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,40 +1,24 @@
-import asyncHandler from '../middleware/asyncHandler.js';
-import User from '../models/User.js';
-import generateToken from '../utils/generateToken.js';
-
-export const login = asyncHandler(async (req, res) => {
+export const register = asyncHandler(async (req, res, next) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      username: user.username,
-      role: user.role,
-      token: generateToken(user._id)
-    });
-  } else {
-    res.status(401);
-    throw new Error('Нэвтрэх нэр эсвэл нууц үг буруу');
-  }
-});
-
-export const register = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
-  const userExists = await User.findOne({ username });
-  if (userExists) {
-    res.status(400);
-    throw new Error('Бүртгэлтэй хэрэглэгч байна');
-  }
-  const user = await User.create({ username, password });
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      username: user.username,
-      role: user.role,
-      token: generateToken(user._id)
-    });
-  } else {
-    res.status(400);
-    throw new Error('Бүртгэл амжилтгүй');
+  try {
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      res.status(400);
+      throw new Error('Бүртгэлтэй хэрэглэгч байна');
+    }
+    const user = await User.create({ username, password });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        role: user.role,
+        token: generateToken(user._id)
+      });
+    } else {
+      res.status(400);
+      throw new Error('Бүртгэл амжилтгүй');
+    }
+  } catch (err) {
+    next(err);
   }
 });
